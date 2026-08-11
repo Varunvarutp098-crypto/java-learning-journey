1da3804fc37e61f66a476b3c4175702c26e299cc
varun <varunvarutp098@gmail.com>
2026-08-11 23:49:39 +0530

    2 code files added

---
+++ variables.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+public class variables {
+    public static void main(String[] args){
+
+        int age = 30, temprature = 35;
+        byte aged = 45;
+        long views_count = 123_321_25_2514_555L;
+        age = 354;
+        float price = 15.55F;
+        char letter = 'v';
+        boolean iseligible = false;
+        byte year = 20;
+        byte v =34;
+        byte g = v ;
+        v = 54;
+        System.out.println(g);
+        System.out.println(v);
+        Date now = new Date();
+        System.out.println(now);
+        System .out.println(age);
+        System.out.println(temprature);
+        System.out.println(views_count);
+    }
+}
+
+
