2c6fc813f4a16e606f75bdb3d614fce574864c23
varun <varunvarutp098@gmail.com>
2026-08-19 12:00:51 +0530

    day6

---
+++ comparison_operator.java
@@ -0,0 +1,7 @@
+public class comparison_operator {
+    public static void main(String[] args){
+        String x = "varun";
+       String y = "varun";
+        System.out.println(x != y);
+    }
+}
