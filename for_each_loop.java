5275abc8f515d9d2372fb3d5d0bca95b3702e2e0
varun <varunvarutp098@gmail.com>
2026-08-21 10:28:42 +0530

    day8

---
+++ for_each_loop.java
@@ -0,0 +1,11 @@
+public class for_each_loop {
+    public static void main(String[] args){
+        String[] fruits = { "apple","mango","orange"};
+        for (int i = 0 ; i < fruits.length; i++)
+          System.out.println(fruits[i]);
+
+        for (String fruit : fruits)
+            System.out.println(fruit);
+
+    }
+}
