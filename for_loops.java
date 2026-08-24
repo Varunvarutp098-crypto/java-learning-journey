89a4ecabe45552c03d2fd46821748bccc9175c18
varun <varunvarutp098@gmail.com>
2026-08-24 23:24:54 +0530

    day9

---
+++ for_loops.java
@@ -0,0 +1,9 @@
+public class for_loops {
+    public static void main(String[] args){
+        for (int i = 0 ; i < 6 ; i++ )
+            System.out.println("hello");
+        for (int i = 5 ; i >= 0; i--)
+            System.out.println("varun" + i);
+
+    }
+}
