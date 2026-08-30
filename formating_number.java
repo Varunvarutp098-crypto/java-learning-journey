965f32c09c424fbde51e815964ea70a0cb27c5f2
VARUN.T.P <varunvarutp098@gmail.com>
2026-08-30 23:53:40 +0530

    Update formating_number.java

--- formating_number.java
+++ formating_number.java
@@ -3,7 +3,7 @@ import java.text.NumberFormat;
 public class formating_number {
         public static void main(String[] args){
            NumberFormat currency =  NumberFormat.getCurrencyInstance();
-            String result = currency.format(1445.5535);
+            String result = currency.format(1445.55);
             System.out.println(result);
             NumberFormat percent =  NumberFormat.getPercentInstance();
             String results = percent.format(0.02);
